159e59f6474ff005c5b968b59e12489c1e06fd6e
wii <lomugdanglf.19@gmail.com>
2026-04-12 22:41:10 +0800

    add job route

--- backend/app/routers/jobs.py
+++ backend/app/routers/jobs.py
@@ -1,5 +1,6 @@
 from database import get_cursor, get_db
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from routers.auth import get_current_user
 
@@ -14,6 +15,15 @@ class Jobs(BaseModel):
     cron_schedule: str
     timeout_seconds: int
     is_active: bool
+
+
+class AddJob(BaseModel):
+    name: str
+    description: str | None = None
+    script_path: str
+    cron_schedule: str
+    timeout_seconds: int | None = 300
+    is_active: bool | None = True
 
 
 @router.get("/")
@@ -33,3 +43,48 @@ def get_jobs(current_user=Depends(get_current_user), conn=Depends(get_db)):
             jobs.append(job)
 
         return jobs
+
+
+@router.post("/add")
+def add_job(body: AddJob, current_user=Depends(get_current_user), conn=Depends(get_db)):
+    user_id = current_user["user_id"]
+    job_name = body.name
+    job_description = body.description
+    script_path = body.script_path
+    cron_schedule = body.cron_schedule
+    timeout_seconds = body.timeout_seconds
+    is_active = False
+
+    with get_cursor(conn) as cur:
+        try:
+            cur.execute(
+                "SELECT id FROM jobs WHERE user_id = %s AND name = %s",
+                (user_id, job_name),
+            )
+            check_dup = cur.fetchall()
+            if check_dup:
+                raise HTTPException(status_code=409, detail="Job Duplicate")
+                return JSONResponse(status=409, content={"message": "Job Duplicate"})
+
+            cur.execute(
+                """
+                    INSERT INTO jobs (user_id, name, description, script_path, cron_schedule, timeout_seconds, is_active) 
+                    VALUES (%s, %s, %s, %s, %s, %s, %s)
+                    RETURNING id;
+                """,
+                (
+                    user_id,
+                    job_name,
+                    job_description,
+                    script_path,
+                    cron_schedule,
+                    timeout_seconds,
+                    is_active,
+                ),
+            )
+            new_job_id = cur.fetchone()["id"]
+            conn.commit()
+            return {"message": "Job added successfully", "job_id": new_job_id}
+        except Exception as e:
+            conn.rollback()
+            raise HTTPException(status_code=500, detail=f"Failed to add job: {str(e)}")
